2103aeac68e95ac21490ffce6c8e9b84ec07e7de
GeorgianaMaria94 <49163195+GeorgianaMaria94@users.noreply.github.com>
2019-08-09 18:51:46 +0300

    Add files via upload

---
+++ Login/Login.js
@@ -0,0 +1,40 @@
+//get modal
+var modal = document.getElementById('display-modal');
+// close the modal if the user click outside of if
+document.getElementById('display-modal').addEventListener("click", function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+})
+
+
+//close the modal if the "cancel" button is clicked
+var cancelBtn = document.querySelector(".cancel-btn");
+cancelBtn.onclick = function (event) {
+    modal.style.display = "none";
+}
+
+var submitBtn = document.querySelector('.submit-btn')
+//Register implementation
+submitBtn.addEventListener("click", function () {
+    var username = document.getElementById('userName').value;
+    var password = document.getElementById('passWord').value;
+    var user = new Auth();
+
+    user.loginFromAPI(username,password).then(function(data) {
+        console.log(data);
+        localStorage.setItem("accessToken", data.accessToken);
+        localStorage.setItem("username", username);
+        
+    }).catch(function(response) {
+        if(response.status=401) {
+            var loginError = document.getElementById("status");
+            loginError.innerHtml = "User not found/wrong password.";
+        }
+    });
+});
+
+
+
+
+
